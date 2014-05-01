53393a747da34060434448584ec281b9a88666d3
drammopo <drammopo@gmail.com>
2014-05-01 23:38:30 +0200

    Added specs

--- scrabble/lib/scrabble.rb
+++ scrabble/lib/scrabble.rb
@@ -1,3 +1,37 @@
 class Scrabble
 
+LETTER_SCORE = {
+  "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+  "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+  "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+  "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+  "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+  "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+  "Y"=>4, "Z"=>10
+}
+
+  def self.score(word)
+    return 0 if word == nil
+    return 0 if word.empty?
+    score_word(word)
+  end
+
+  def self.highest_score_from(words)
+    score_hash = words.inject({}) do |h, (word)|
+      h[word] = score_word(word)
+      h
+    end
+    sorted_score_hash = score_hash.sort_by { |k,v| v}
+    highest_scoring_pair = sorted_score_hash.last
+    highest_score = highest_scoring_pair.last
+    highest_scoring_pair.first
+  end
+
+  private
+
+  def self.score_word(word)
+    formatted_word = word.upcase
+    formatted_word.split("").inject(0) { |total,letter| total + LETTER_SCORE.fetch(letter) }
+  end
+
 end
--- scrabble/spec/scrabble_spec.rb
+++ scrabble/spec/scrabble_spec.rb
@@ -6,5 +6,31 @@ describe Scrabble do
       expect( Scrabble.score("a") ).to eq 1
       expect( Scrabble.score("f") ).to eq 4
     end
+
+    it "scores an empty word" do
+      expect( Scrabble.score("") ).to eq 0
+    end
+
+    it "scores a nil" do
+      expect( Scrabble.score(nil) ).to eq 0
+    end
+
+    it "scores a word" do
+      expect( Scrabble.score("hello") ).to eq 8
+    end
+  end
+
+  describe ".highest_score_from" do
+    it "scores a single letter" do
+      expect( Scrabble.highest_score_from(['home', 'word', 'hello', 'sound']) ).to eq 'home'
+    end
+
+    it "picks the highest score that uses all seven letters" do
+      expect( Scrabble.highest_score_from(['home', 'word', 'silence']) ).to eq 'silence'
+    end
+
+    it "picks the first one in supplied list in if the there are multiple words that are the same score and same length" do
+      expect( Scrabble.highest_score_from(['hi', 'word', 'ward']) ).to eq 'word'
+    end
   end
 end
