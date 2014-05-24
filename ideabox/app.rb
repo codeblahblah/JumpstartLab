d7975df84b4e39ea3d65d3a2a0dfb5f04db51ba4
drammopo <drammopo@gmail.com>
2014-05-24 14:51:03 +0200

    Added update route

--- ideabox/app.rb
+++ ideabox/app.rb
@@ -15,6 +15,11 @@ class IdeaBoxApp < Sinatra::Base
     erb :index, locals: {ideas: Idea.all}
   end
 
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
   post '/' do
     idea = Idea.new(params['idea_title'], params['idea_description'])
     idea.save
@@ -22,7 +27,16 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   delete '/:id' do |id|
-    Idea.delete(id)
+    Idea.delete(id.to_i)
+    redirect '/'
+  end
+
+  put '/:id' do |id|
+    data = {
+      :title => params['idea_title'],
+      :description => params['idea_description']
+    }
+    idea = Idea.update(id.to_i, data)
     redirect '/'
   end
 end
--- ideabox/idea.rb
+++ ideabox/idea.rb
@@ -35,4 +35,26 @@ class Idea
     Idea.database
   end
 
+  def self.delete(position)
+    database.transaction do
+      database['ideas'].delete_at(position)
+    end
+  end
+
+  def self.find_raw_idea(id)
+    database.transaction do
+      database['ideas'].at(id)
+    end
+  end
+
+  def self.find(id)
+    raw_idea = find_raw_idea(id)
+    new(raw_idea[:title], raw_idea[:description])
+  end
+
+  def self.update(id, data)
+    database.transaction do
+      database['ideas'][id] = data
+    end
+  end
 end
