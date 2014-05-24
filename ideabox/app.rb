911ff969f4a6a7c42cc3970266f10b18df858c1f
drammopo <drammopo@gmail.com>
2014-05-24 13:10:16 +0200

    Added #save

--- ideabox/app.rb
+++ ideabox/app.rb
@@ -1,3 +1,5 @@
+require_relative './idea'
+
 class IdeaBoxApp < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
@@ -12,6 +14,11 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   post '/' do
+    # 1. Create an idea based on the form parameters
+    idea = Idea.new
+    # 2. Store it
+    idea.save
+    # 3. Send us back to the index page to see all ideas
     "Creating an IDEA!"
   end
 end
---
+++ ideabox/idea.rb
@@ -0,0 +1,15 @@
+require 'yaml/store'
+
+class Idea
+
+  def save
+    database.transaction do |db|
+      db['ideas'] ||= []
+      db['ideas'] << {title: 'diet', description: 'pizza all the time'}
+    end
+  end
+
+  def database
+    @database ||= YAML::Store.new "ideabox"
+  end
+end
