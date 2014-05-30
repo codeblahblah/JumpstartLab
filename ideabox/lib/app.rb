ecde834b3e69f5edaf58ab294fb83e9607ab4747
drammopo <drammopo@gmail.com>
2014-05-30 05:46:07 -0700

    Extensions-stats: Added routes and pointed layout to them

--- ideabox/lib/app.rb
+++ ideabox/lib/app.rb
@@ -12,6 +12,12 @@ class IdeaBoxApp < Sinatra::Base
     haml :error
   end
 
+
+  delete '/:id' do |id|
+    IdeaStore.delete(id.to_i)
+    redirect '/'
+  end
+
   get '/' do
     haml :index, locals: {ideas: IdeaStore.all.sort, idea: Idea.new}
   end
@@ -21,19 +27,17 @@ class IdeaBoxApp < Sinatra::Base
     haml :edit, locals: {idea: idea}
   end
 
+  get '/slot/:id' do |id|
+    "Viewing IDEAS by time slot!"
+  end
+
+  get '/day/:id' do |id|
+    "Viewing IDEA by day!"
+  end
+
   post '/' do
     idea = Idea.new(params[:idea])
     idea.save
-    redirect '/'
-  end
-
-  delete '/:id' do |id|
-    IdeaStore.delete(id.to_i)
-    redirect '/'
-  end
-
-  put '/:id' do |id|
-    IdeaStore.update(id.to_i, params[:idea])
     redirect '/'
   end
 
@@ -41,6 +45,11 @@ class IdeaBoxApp < Sinatra::Base
     idea = IdeaStore.find(id.to_i)
     idea.like!
     IdeaStore.update(id.to_i, idea.to_h)
+    redirect '/'
+  end
+
+  put '/:id' do |id|
+    IdeaStore.update(id.to_i, params[:idea])
     redirect '/'
   end
 end
